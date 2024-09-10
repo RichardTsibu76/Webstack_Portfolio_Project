b923565c08126d6a66c33c38ec0f5c57ef043e4a
EnduranceApeh <apehendurance2@gmail.com>
2024-09-10 15:02:47 +0100

    made changes to the context file to handle data fetching

--- hotel-booking-app/src/context.js
+++ hotel-booking-app/src/context.js
@@ -49,7 +49,6 @@ class RoomProvider extends Component {
    this.getData()
   }
 
-  // data formating
   formatData(items) {
     let tempItems = items.map(item => {
       let id = item.sys.id
@@ -67,4 +66,69 @@ class RoomProvider extends Component {
     const room = tempRooms.find(room => room.slug === slug);
     return room;
   }
+  handleChange = event => {
+    const target = event.target
+    const value = target.type === "checkbox" ?target.checked:target.value
+    const name = event.target.name
+    this.setState({
+      [name]:value
+    },
+    this.filterRooms
+  )
+  }
+  filterRooms = ()=>{
+    let{
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state
+    // all the rooms
+    let tempRooms = [...rooms]
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price)
+
+    // filter by type
+    if(type !== 'all'){
+      tempRooms = tempRooms.filter(room => room.type === type)
+    }
+
+    // filter by capacity
+    if(capacity !== 1){
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+    }
+    // filer by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+    // filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+    // filter by breakfast
+    if(breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+    //filter by pets
+    if(pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
+    // change state
+    this.setState({
+      sortedRooms:tempRooms
+    })
+  }
+
+  render() {
+    return (
+      <RoomContext.Provider value={{...this.state,getRoom:this.getRoom,handleChange:this.handleChange}}>
+        {this.props.children}
+      </RoomContext.Provider>
+    )
+  }
 }
+
+const RoomConsumer = RoomContext.Consumer;
+
+export {RoomProvider, RoomConsumer, RoomContext}
